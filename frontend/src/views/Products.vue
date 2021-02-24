2de1be5c61aaf9ed238503dcb09e613eb52d5eb5
Leon Wass Vallin <leon.wass.vallin@gmail.com>
2021-02-24 07:38:54 +0100

    added header and footer component to products page.

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
+    <Header />
+    <main>
       <h1>Products</h1>
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 export default {
-
-}
+  components: { Header, Footer },
+};
 </script>
 
-<style>
-
+<style scoped>
+main {
+  /* Bara tillfälligt för att sära på header och footer */
+  height: 80vh;
+  background: rgb(219, 120, 39);
+}
 </style>
