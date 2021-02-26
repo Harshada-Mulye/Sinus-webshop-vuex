284e01cbc5d3e342cfebaca4bd30e8303aafe944
noppeforslin@gmail.com <noppeforslin@gmail.com>
2021-02-26 18:09:06 +0100

    Informationsruta

---
+++ frontend/src/components/Productinfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="background-container">
+      <div class="img-div">
+        <img src="hoodie-ash.png">
+      </div>
+      <h1>Green Hoodie</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.background-container{
+    width: 700px;
+    height: 400px;
+    border: solid black;
+    border-radius: 20px;
+    background-color: black;
+    margin-left: 10px;
+    opacity: 0.9;
+    
+}
+.img-div{
+  height: 2px;
+  width: 10px;
+}
+h1{
+  font-family: 'Courier New', Courier, monospace;
+}
+
+</style>
