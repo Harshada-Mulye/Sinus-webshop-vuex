0d1be843b212cca6d52abbe9b0213491668cecc1
lewava <leon.wass.vallin@gmail.com>
2021-02-26 12:10:16 +0100

    fixed so database don't reset on start

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -53,7 +53,7 @@ export default {
             console.log(response.data)
         },
         async postOrder(context, payload) {
-            const response = await API.postOrder(payload);
+            const response = await API.postOrders(payload);
             console.log(response);
         }
     }
