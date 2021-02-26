4ab86aaff8232edf8df6b0593f38602573de526e
noppeforslin@gmail.com <noppeforslin@gmail.com>
2021-02-26 18:23:35 +0100

    tog bort rutan

--- frontend/src/components/Productinfo.vue
+++ frontend/src/components/Productinfo.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="background-container">
-      <div class="img-div">
-        <img src="hoodie-ash.png">
-      </div>
-      <h1>Green Hoodie</h1>
-  </div>
+<div>
+  
+</div>
+  
 </template>
 
 <script>
@@ -14,22 +12,6 @@ export default {
 </script>
 
 <style>
-.background-container{
-    width: 700px;
-    height: 400px;
-    border: solid black;
-    border-radius: 20px;
-    background-color: black;
-    margin-left: 10px;
-    opacity: 0.9;
-    
-}
-.img-div{
-  height: 2px;
-  width: 10px;
-}
-h1{
-  font-family: 'Courier New', Courier, monospace;
-}
+
 
 </style>
